5245d4b83a29dac8349e0a81b9e34572a15da17a
Sourav Soni <sauravsoni8980@gmail.com>
2023-04-19 09:16:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full-Stack Developer", "Freelancer", "Programmer", "Web Designer", "Video Editor"],
+        strings: ["Full-Stack Developer", "Freelancer", "Programmer", "Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full-Stack Developer", "Freelancer", "Programmer", "Web Designer", "Video Editor"],
+        strings: ["Full-Stack Developer", "Freelancer", "Programmer", "Web Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
